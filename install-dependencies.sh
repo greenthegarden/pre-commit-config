91927c8f9825e0dc8832897d18f9567624524af3
Philip Cutler <greenthegarden@gmail.com>
2021-04-17 23:09:58 +0930

    support configuring poetry project

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,20 +1,35 @@
 #!/usr/bin/env bash
 
+PYTHON_PROJECT=true
+MODULES=""
+
 # install pip
 sudo apt-get install -y git python3-pip
 
 # install base python modules
-python3 -m pip install pip pipx --upgrade
+python3 -m pip install --upgrade pip pipx
 python3 -m pipx ensurepath
 
 # now use pipx to install the rest
-pipx install ansible # IT automation
-pipx install molecule # Testing
 pipx install pre-commit
 pipx install yamllint # YAML lint tool
-pipx install ansible-lint # Ansible lint tool
-pipx install poetry # Python package manager
+pipx install poetry # Ansible lint tool
 
 # install git hook scripts
-git init
+if [ ! -d .git ]; then
+    git init
+fi
 pre-commit install
+
+if [ "$PYTHON_PROJECT" = "true" ]; then
+    PROJECTNAME="${PROJECTNAME:=$(basename "${PWD}")}"
+    # install packages via poetry
+    if [ ! -f pyproject.toml ]; then
+        poetry new ${PROJECTNAME}
+    fi
+
+    # install modules
+    if [ "$MODULES" ]; then
+        current_dir=$PWD;cd ${PROJECTNAME};poetry add {MODULES};cd $current_dir;
+    fi
+fi
